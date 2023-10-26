9809f422bf21f26f6999430410188a79d192e87b
henry <cardona.serna310@gmail.com>
2023-10-26 11:13:26 -0500

    se agrego la información de nosotros

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
-function cambiarContenido() {
-    var selectElement = document.getElementById("select");
-    var selectedOption = selectElement.value;
+function redirigirPagina() {
 
-    // Ocultar todos los contenidos
-    document.getElementById("contenidoComunidad").style.display = "none";
-    document.getElementById("contenidoEstudiante").style.display = "none";
-    document.getElementById("contenidoAuxiliar").style.display = "none";
+    const select = document.getElementById('select');
 
-    // Ocultar todos los menús
-    document.getElementById("menuAuxiliar").style.display = "none";
-    document.getElementById("menuEstudiante").style.display = "none";
+    const opcionSeleccionada = select.value;
 
-    // Mostrar el contenido y el menú correspondientes a la selección
-    if (selectedOption === "Estudiante") {
-        document.getElementById("contenidoEstudiante").style.display = "block";
-        document.getElementById("menuEstudiante").style.display = "block";
-    } else if (selectedOption === "Auxiliar de Cocina") {
-        document.getElementById("contenidoAuxiliar").style.display = "block";
-        document.getElementById("menuAuxiliar").style.display = "block";
-    } else {
-        document.getElementById("contenidoComunidad").style.display = "block";
+    if (opcionSeleccionada === 'Estudiante') {
+        window.location.href = '/EstudianteHTML/inicioo.html';
+    } else if (opcionSeleccionada === 'Auxiliar de Cocina') {
+        window.location.href = '/AuxiliarHTML/inicioo.html';
     }
+    return false;
 }
+
 const sign_in_btn= document.querySelector("#sign-in-btn");
 const sign_up_btn= document.querySelector("#sign-up-btn");
 const container= document.querySelector(".container");
